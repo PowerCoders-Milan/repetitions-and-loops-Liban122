c15635ca757793d5c88c8d023bded31c34a9889c
liban <botanlee515@gmail.com>
2021-03-01 13:39:16 +0100

    Done

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,10 @@
 // write your code below
+var x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x.forEach(function(y){
+    document.write(y + '*' + 9+ '=' +y*9+ '<br>')
+})
+
+function multi9(num){
+    return num * 9
+}
+document.write(multi9(10))
