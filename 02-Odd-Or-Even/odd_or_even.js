a3e17cfa49166e888cec1f53b5edc27496054694
liban <botanlee515@gmail.com>
2021-03-01 12:17:50 +0100

    Done

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,38 @@
   // check if the current is even or odd
 
   // report the result to the screen
+  
+for(i = 0; i <= 20; i++){
+  
+  if(i % 2 === 0){
+    console.log(i)
+  }
+
+  else{
+    console.log('Its odd')
+  }
+}
+
+var x = 0
+while(x <= 20){
+  if(x % 2 === 0){
+    console.log(x)
+  }
+
+  else{
+    console.log('Its odd')
+  }
+  x++
+}
+
+
+do{
+  if(x % 2 === 0){
+    console.log(x)
+  }
+
+  else{
+    console.log('Its odd')
+  }
+  x++
+} while(x <= 20)
